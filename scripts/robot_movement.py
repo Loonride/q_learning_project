b4c70d1988bc4376d1e26fece67668ea58d2fc4c
Sydney Jenkins <sydneyjenkins@hitmonchan.cs.uchicago.edu>
2021-05-09 20:21:42 -0500

    debugging

--- scripts/robot_movement.py
+++ scripts/robot_movement.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import rospy
-import numpy as np
-
-import cv2, cv_bridge
-from std_msgs.msg import Empty
+import rospy, cv2, cv_bridge, numpy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+import numpy as np
+
+from std_msgs.msg import Empty
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Vector3
 from q_learning_project.msg import RobotMoveDBToBlock
@@ -44,19 +43,13 @@ class RobotMovement(object):
         self.action_queue.append(data)
 
     def getDistance(self, msg):
-        print("inside get distance")
         self.front_distance = msg.ranges[0]
 
     def complete_action(self, msg):
-        print("INSIDE")
         if (not self.initalized):
-            self.set_v(0,0)
-            print("returning")
             return
 
         if len(self.action_queue) == 0:
-            self.set_v(0,0)
-            print("no actions to do")
             return
 
         if self.carrying_db:
@@ -67,7 +60,7 @@ class RobotMovement(object):
         color = target.robot_db # "red", "blue", "green"
         self.block_id = target.block_id # 1, 2, 3
 
-        self.dumbbell_target = np.where(np.asarray(["red","green","blue"]==color))[0]
+        self.dumbbell_target = np.where(np.asarray(["red","green","blue"])==color)[0][0]
 
         # converts the incoming ROS message to OpenCV format and HSV (hue, saturation, value)
         image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
@@ -75,12 +68,13 @@ class RobotMovement(object):
 
         # define the upper and lower bounds rgb
         lower_bounds = np.array([0, 121/2, 241/2]) 
-        upper_bounds = np.array([60/2, 180/2, 300/2])
-        rgb_lower = [[lower_bounds[i],50, 50] for i in range(3)]
-        rgb_upper = [[upper_bounds[i],255, 255] for i in range(3)]
-
+        upper_bounds = np.array([300000, 180/2, 300/2])
+        rgb_lower = [np.asarray([lower_bounds[i],20, 20]) for i in range(3)]
+        rgb_upper = [np.asarray([upper_bounds[i],255, 255]) for i in range(3)]
+        
+        print("target:", rgb_lower[self.dumbbell_target])
         mask = cv2.inRange(hsv, rgb_lower[self.dumbbell_target], rgb_upper[self.dumbbell_target])
-
+        print("Mask:",mask)
         # this erases all pixels that aren't desired color
         h, w, d = image.shape
         search_top = int(3*h/4)
@@ -102,19 +96,18 @@ class RobotMovement(object):
 
                 err = w/2 - cx
                 k_p = 1.0 / 100.0
-                print(err)
                 if err > .05: 
-                    self.set_v(0, k_p*err)
+                    self.set_v(.2, k_p*err)
                 elif self.front_distance > .8: 
                     self.set_v(.2, k_p*err)
                 else: 
                     self.set_v(0,0)
                     #pick up dumbbell
                     self.carrying_db = True
-
+        else: 
+            self.set_v(0,.2)
         #cv2.imshow("window", image)
         #cv2.waitKey(3)
-        return
 
     def find_number(self, msg):
         """ Find block with target ID
